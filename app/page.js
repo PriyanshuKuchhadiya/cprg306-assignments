63abb7b954b3600d41f41126bb8d0495bfadfebd
priyanshu000920195 <priyanshukanabhai.kuchhadiya@edu.sait.ca>
2024-09-24 08:12:27 -0600

    delete week 2

--- app/page.js
+++ app/page.js
@@ -8,6 +8,30 @@ export default function StudentInfo() {
         <Link href="\week-2" target="_blank">Week 2
         </Link>
       </p>
+      <p>
+      <Link href="\week-3" target="_blank">Week 3
+      </Link>
+      </p>
+      <p>
+      <Link href="\week-4" target="_blank">Week 4
+      </Link>
+      </p>
+      <p>
+      <Link href="\week-5" target="_blank">Week 5
+      </Link>
+      </p>
+      <p>
+      <Link href="\week-6" target="_blank">Week 6
+      </Link>
+      </p>
+      <p>
+      <Link href="\week-7" target="_blank">Week 7
+      </Link>
+      </p>
+      <p>
+      <Link href="\week-8" target="_blank">Week 8
+      </Link>
+      </p>
     </div>
   );
 }
